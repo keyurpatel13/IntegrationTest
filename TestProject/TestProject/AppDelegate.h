f501c75c60a4a9c92d4542c44ac704419abade35
Keyur Patel <keyur_cancer13@yahoo.com>
2014-08-25 15:13:28 +0100

    initial project setup

---
+++ TestProject/TestProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by Keyur Patel on 25/08/2014.
+//  Copyright (c) 2014 Keyur Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestProject/TestProject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestProject
+//
+//  Created by Keyur Patel on 25/08/2014.
+//  Copyright (c) 2014 Keyur Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
